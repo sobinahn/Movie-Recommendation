201b10e1a1ee2cce41a747dd8e250302d6b220d9
sophiejjam <64030326+sophiejjam@users.noreply.github.com>
2020-04-28 02:58:26 +0900

    optimization

--- MAKE.py
+++ MAKE.py
@@ -1,4 +1,9 @@
 from openpyxl import load_workbook
 
-wb = load_workbook('movie_optimization.xlsx')
-print(wb.sheetnames())
+print('Opening results from the workbook...')
+
+wb = load_workbook(filename = 'movie_optimization.xlsm', read_only=True)
+ws = wb['result']
+for row in ws.rows:
+    for cell in row:
+        print(cell.value)
