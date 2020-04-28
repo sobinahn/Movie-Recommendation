78a332f7b61e1d1041df38928f0c9d590f7fc1bc
sophiejjam <64030326+sophiejjam@users.noreply.github.com>
2020-04-29 02:23:04 +0900

    Update MAKE.py
    The code can now get genre from the genre API. (no need to print but currently included)

--- MAKE.py
+++ MAKE.py
@@ -1,29 +1,109 @@
-import pandas as pd
+APIKey = "05ff55684cb55f443d41d5558c15d6bb"
 
-print('Please enter your preferences with digits between 0 to 10.')
+import json, requests, pprint as pr, time, pandas as pd, numpy as np
 
-comedy = input('Comedy ')
-action = input('Action ')
-thriller = input('Thriller ')
-romance = input('Romance ')
-scifi = input('Sci-fi ')
-reality = input('Reality ')
-documentary = input('Documentary ')
-kids = input('Kids ')
-animation = input('Animation ')
-horror = input('Horror ')
-drama = input('Drama ')
 
-genres = pd.Series({'Comedy': comedy,
-                    'Action': action,
-                    'Thriller': thriller,
-                    'Romance': romance,
-                    'Sci-fi': scifi,
-                    'Reality': reality,
-                    'Documentary': documentary,
-                    'Kids': kids,
-                    'Animation': animation,
-                    'Horror': horror,
-                    'Drama': drama})
+def get_API():
+    print('Please get your API key from https://developers.themoviedb.org/3/getting-started/introduction')
+    print('Please enter your API key from TMDB: ', end = '')
+    API = input()
+    return API
 
-print(genres.head(11))
+genre = 'Action Adventure Animation Comedy Crime Documentary Drama Family Fantasy History Horror Music Mystery Romance Science Fiction TV Movie Thriller War Western'.split()
+
+def get_MovieList(API):
+# This function will ask recent movies/TV shows users watched and liked.
+# Users will provide title of the movies/TV shows, and provide rating to them.
+
+    print('> To recommend you movies, I need to know list of films and TV shows you recently enjoyed!')
+    #time.sleep(1)
+    print('> More films you tell me, the better recommendation is going to be.''')
+    #time.sleep(1)
+    print('> How many favorite films do you want to tell me:' ,end='')
+    numberofObjects = input()
+    numberofObjects = int(numberofObjects)
+
+    # Taking keywords user want to look for
+    print('> Please enter the title of your favorite film one by one!')
+    user_movieList = []
+    user_movieID = []
+    for i in range(numberofObjects):
+        print('Name of the film (with spaces between words): ', end='')
+        title_search = input()
+        url = 'https://api.themoviedb.org/3/search/movie?api_key=%s&language=en-US&query=%s&page=1&include_adult=true'%(API, title_search)
+        response = requests.get(url)
+        response.raise_for_status()
+        jsonData = json.loads(response.text)
+        if jsonData['total_results'] > 1:
+            print("There are multiple films that matches your search which is the one...")
+            title = []
+            release_date = []
+            overview = [] # plot
+            movie_ID = [] # Unique number for a film
+
+            for t in jsonData['results']:
+                title.append(t['title'])
+                try:
+                    release_date.append(str(t['release_date']))
+                except:
+                    release_date.append('NA')
+                overview.append(t['overview'])
+                try:
+                    movie_ID.append(t['id'])
+                except:
+                    movie_ID.append('NA')
+
+            userDF = pd.DataFrame({'title': title,
+                         'release_date': release_date, 'movie_ID': movie_ID})
+            pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+            # Verify_list is a Dataframe that contains the title and the release date of films that users selected
+            verify_list = pd.DataFrame({'title': title,
+                         'release_date': release_date})
+                         #'overview': overview})
+            print(verify_list)
+            print('Please enter the ID number of a film you\'re looking for: ', end='')
+
+            idNumber = int(input())
+            title_df = userDF.get('title')
+            id_df = userDF.get('movie_ID')
+            user_movieList.append(title_df[idNumber])
+            user_movieID.append(id_df[idNumber])
+
+            #Creating a DataFrame that contains the titles and IDs for films
+            user_response = pd.DataFrame({'Title': user_movieList, 'Movie ID': user_movieID})
+
+        print('> Movies you\'re searching for: %s' % user_movieList)
+    print(user_response)
+
+    return user_response
+
+def get_MovieRating(movie_DF):
+    print('Now, can you please give rating for the movies you mentioned?')
+    user_rating = []
+    movie_titles = movie_DF.get('Title')
+
+    for i in range(len(movie_titles)):
+        print('What is your rating for the film %s: ' %(movie_titles[i]), end = '')
+        user_rating.append(input())
+    rating_DF = pd.DataFrame({'User Rating': user_rating})
+    DF = pd.concat([movie_DF, rating_DF], axis=1)
+
+    return DF
+
+API = get_API()
+movie_DF = get_MovieList(API)
+userDF = get_MovieRating(movie_DF)
+print(userDF)
+
+
+def get_Genre(API):
+    url2 = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API + '&language=en-US'
+    response = requests.get(url2)
+    response.raise_for_status()
+    jsonData = json.loads(response.text)
+    print(jsonData)
+
+    return jsonData
+
+get_Genre(API)
